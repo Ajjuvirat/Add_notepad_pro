49f116f8550b2f23f3fe5101a9d70f3419453d1c
Ajju <ajjuvirat1995@gmail.com>
2025-06-30 14:28:17 +0530

    Added Add.java file form notepad

---
+++ Add.java
@@ -0,0 +1,8 @@
+public class Add {
+    public static void main(String args []){
+          int a =8; 
+          int b =6;
+          int c = a+b;
+    System.out.println("Add number : " +c);
+    }
+   }
